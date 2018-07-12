e778009a300ff9a3fae8996c2d932d0281f81012
hiro <hiroto.abc.u@gmail.com>
2018-07-12 21:03:38 +0900

    edit newPost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+    before_action :get_new_posts
+    def get_new_posts
+        @new_posts = Post.all.order(created_at: :desc).limit(6)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'home#index'
+  root :to => 'posts#index'
 end
