0e2bec021cf5e5e874be0fdb9649c9331ab600b1
hiro <hiroto.abc.u@gmail.com>
2018-07-24 18:25:31 +0900

    drop unnecessary Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_125256) do
-
-  create_table "categories", force: :cascade do |t|
-    t.integer "category_id"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
+ActiveRecord::Schema.define(version: 2018_07_24_081715) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
